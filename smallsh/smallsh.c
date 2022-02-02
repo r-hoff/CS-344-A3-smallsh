8c49a992a8344e22590feb1af41b1bc9d5c7bde3
Ryan Hoff <ryanchoff@gmail.com>
2022-02-01 22:47:26 -0800

    get user input

--- smallsh/smallsh.c
+++ smallsh/smallsh.c
@@ -26,9 +26,99 @@ char* getInput()
 	return userInput;
 }
 
+struct commandLine
+{
+	char* command;
+	char* arguments;
+	char* inputFile;
+	char* outputFile;
+	int backgroundFlag;
+};
+
+struct commandLine* createCommandLine()
+{
+	struct commandLine* currCommand = malloc(sizeof(struct commandLine));
+	currCommand->backgroundFlag = 0;
+
+	char* saveptr;
+	char* line = getInput();
+	char* token = strtok_r(line, " ", &saveptr);
+
+	if (strcmp(token, "\n") == 0 || token[0] == "#"[0])
+	{
+		currCommand->command = NULL;
+		currCommand->arguments = NULL;
+		currCommand->inputFile = NULL;
+		currCommand->outputFile = NULL;
+	}
+	else
+	{
+		currCommand->command = calloc(strlen(token) + 1, sizeof(char));
+		strcpy(currCommand->command, token);
+
+		// handle & at end of input if exists
+		token = saveptr;
+		if (token[strlen(token) - 2] == "&"[0])
+		{
+			currCommand->backgroundFlag = 1;
+			token[strlen(token) - 3] = '\0';
+			currCommand->arguments = calloc(strlen(token) + 1, sizeof(char));
+			strcpy(currCommand->arguments, token);
+		}
+		else
+		{
+			currCommand->arguments = calloc(strlen(token) + 1, sizeof(char));
+			strcpy(currCommand->arguments, token);
+		}
+
+		// check for input file arg
+		char* inFile = strstr(saveptr, "< ");
+		if (inFile)
+		{
+			inFile = inFile + 2;
+			token = strtok_r(inFile, " ", &saveptr);
+			currCommand->inputFile = calloc(strlen(token) + 1, sizeof(char));
+			strcpy(currCommand->inputFile, token);
+		}
+		else
+		{
+			currCommand->inputFile = NULL;
+		}
+
+		// check for output file arg
+		char* outFile = strstr(saveptr, "> ");
+		if (outFile)
+		{
+			outFile = outFile + 2;
+			token = strtok_r(outFile, " ", &saveptr);
+			currCommand->outputFile = calloc(strlen(token) + 1, sizeof(char));
+			strcpy(currCommand->outputFile, token);
+		}
+		else
+		{
+			currCommand->outputFile = NULL;
+		}
+	}
+
+	return currCommand;
+}
+
+
+void processInput()
+{
+
+}
+
 int main()
 {
-	char line = getInput();
-	printf("%s", line);
+	pid_t smallshPID = getppid();
+
+	struct commandLine* currCommand = createCommandLine();
+
+	while (strcmp(currCommand->command, "exit") != 0)
+	{
+		currCommand = createCommandLine();
+	}
+
 	return 0;
 }
